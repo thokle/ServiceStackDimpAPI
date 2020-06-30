32422a060ffb6b35e42718dac9dcd095c0d2619b
thokle <thomas_kleist@tdcadsl.dk>
2020-06-30 20:35:40 +0200

    Add project files.

---
+++ ServiceStackDimpAPI.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using ServiceStackDimpAPI.ServiceModel;
+
+namespace ServiceStackDimpAPI.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
---
+++ ServiceStackDimpAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServiceStackDimpAPI.Global" Language="C#" %>
